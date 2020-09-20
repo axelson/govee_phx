a6d092e34221b298d9e23bc15549455c30261202
Jason Axelson <jason.axelson@gmail.com>
2020-09-19 21:59:05 -1000

    Update to new form of govee library

--- lib/govee_phx_application.ex
+++ lib/govee_phx_application.ex
@@ -7,7 +7,7 @@ defmodule GoveePhxApplication do
     children = [
       GoveePhxWeb.Telemetry,
       {Phoenix.PubSub, name: GoveePhx.PubSub},
-      {Govee.H6001Bulb, [type: :usb, configuration: %{vid: 0x0A5C, pid: 0x21E8}]},
+      govee_ble(),
       GoveePhxWeb.Endpoint
     ]
 
@@ -20,5 +20,30 @@ defmodule GoveePhxApplication do
   def config_change(changed, _new, removed) do
     GoveePhxWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  def govee_ble do
+    alias BlueHeron.HCI.Command.ControllerAndBaseband.WriteLocalName
+
+    opts = [
+      devices: [
+        [
+          type: :h6001,
+          addr: 0xA4C138EC49BD
+        ],
+        [
+          type: :h6159,
+          addr: 0xA4C138668E6F
+        ]
+      ],
+      transport_config: %BlueHeronTransportUSB{
+        vid: 0x0A5C,
+        pid: 0x21E8,
+        init_commands: [%WriteLocalName{name: "Govee Controller"}]
+      }
+    ]
+
+    # Govee.BLEConnection.start_link(opts, name: Server)
+    {Govee.BLEConnection, opts}
   end
 end
--- lib/govee_phx_web/live/govee_live.ex
+++ lib/govee_phx_web/live/govee_live.ex
@@ -2,7 +2,8 @@ defmodule GoveePhxWeb.GoveeLive do
   use GoveePhxWeb, :live_view
   require Logger
 
-  alias Govee.H6001Bulb
+  alias Govee.CommonCommands
+  alias Govee.BLEConnection
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
@@ -11,29 +12,47 @@ defmodule GoveePhxWeb.GoveeLive do
 
   @impl Phoenix.LiveView
   def handle_event("off", _, socket) do
-    H6001Bulb.turn_off(H6001Bulb)
+    CommonCommands.turn_off()
+    |> run_command()
+
     {:noreply, socket}
   end
 
   def handle_event("on", _, socket) do
-    H6001Bulb.turn_on(H6001Bulb)
+    CommonCommands.turn_on()
+    |> run_command()
+
     {:noreply, socket}
   end
 
   def handle_event("color", %{"hex" => "#" <> hex_str}, socket) do
     {hex, ""} = Integer.parse(hex_str, 16)
 
-    H6001Bulb.set_color(H6001Bulb, hex)
+    CommonCommands.set_color(hex)
+    |> run_command()
+
     {:noreply, socket}
   end
 
   def handle_event("brightness", %{"value" => value}, socket) do
-    H6001Bulb.set_brightness(H6001Bulb, value)
+    CommonCommands.set_brightness(value)
+    |> run_command()
+
     {:noreply, socket}
   end
 
   def handle_event(event, params, socket) do
     Logger.warn("Unhandled event \"#{event}\" with params: #{inspect(params)}")
     {:noreply, socket}
+  end
+
+  defp run_command(command) do
+    for_each_device(fn device ->
+      CommonCommands.send_command(command, device.att_client)
+    end)
+  end
+
+  defp for_each_device(fun) when is_function(fun, 1) do
+    Enum.each(BLEConnection.connected_devices(Server), fun)
   end
 end
