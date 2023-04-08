cbd62e751bf323f6aaf144fd5587f03b69ba614f
Jason Axelson <jason.axelson@gmail.com>
2023-04-08 10:23:09 -1000

    Add party_mode and all_off functions

--- lib/govee_phx.ex
+++ lib/govee_phx.ex
@@ -6,4 +6,23 @@ defmodule GoveePhx do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def party_mode do
+    GoveePhxApplication.BLESupervisor.get_conns()
+    |> Enum.each(fn conn ->
+
+      command = Govee.Command.set_color(0x8E59FF)
+      GoveePhxApplication.BLESupervisor.execute_command(command, conn)
+
+      command = Govee.Command.turn_on()
+      GoveePhxApplication.BLESupervisor.execute_command(command, conn)
+    end)
+  end
+
+  def all_off do
+    GoveePhxApplication.BLESupervisor.get_conns()
+    |> Enum.each(fn conn ->
+      GoveePhxApplication.BLESupervisor.execute_command(Govee.Command.turn_off(), conn)
+    end)
+  end
 end
