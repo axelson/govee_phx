03c0cab75ccd8e5b0c8cde7229b5527a5783c418
Jason Axelson <jason.axelson@gmail.com>
2023-04-08 10:23:28 -1000

    Update phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule GoveePhx.MixProject do
       {:phoenix_view, "~> 2.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.7"},
+      {:phoenix_live_view, "~> 0.18.16"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7"},
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
