59e6934cdace5c2bbe05ae7729ba9e2960087227
Jason Axelson <jason.axelson@gmail.com>
2026-07-26 08:31:07 -1000

    fix compilation warnings

--- mix.exs
+++ mix.exs
@@ -79,33 +79,33 @@ defmodule GoveePhx.MixProject do
   end
 
   defp dep(:govee, :github), do: {:govee, github: "axelson/govee", branch: "new-update"}
-  defp dep(:govee, :path), do: {:govee, path: "~/dev/govee", override: true}
+  # defp dep(:govee, :path), do: {:govee, path: "~/dev/govee", override: true}
 
   defp dep(:govee_semaphore, :github), do: {:govee_semaphore, github: "axelson/govee_semaphore"}
-  defp dep(:govee_semaphore, :path), do: {:govee_semaphore, path: "~/dev/govee_semaphore"}
+  # defp dep(:govee_semaphore, :path), do: {:govee_semaphore, path: "~/dev/govee_semaphore"}
 
-  defp dep(:govee_scenic, :github), do: {:govee_scenic, github: "axelson/govee_scenic"}
+  # defp dep(:govee_scenic, :github), do: {:govee_scenic, github: "axelson/govee_scenic"}
 
   # defp dep(:govee_scenic, :path, opts \\ []), do: {:govee_scenic, Keyword.merge(path: "~/dev/govee_scenic", opts)}
 
   defp dep(:blue_heron, :hex), do: {:blue_heron, ">= 0.0.0", override: true}
 
-  defp dep(:blue_heron, :github),
-    do: {:blue_heron, github: "blue-heron/blue_heron", branch: "main", override: true}
+  # defp dep(:blue_heron, :github),
+  #   do: {:blue_heron, github: "blue-heron/blue_heron", branch: "main", override: true}
 
-  defp dep(:blue_heron, :path),
-    do: {:blue_heron, path: "~/dev/forks/blue_heron/blue_heron", override: true}
+  # defp dep(:blue_heron, :path),
+  #   do: {:blue_heron, path: "~/dev/forks/blue_heron/blue_heron", override: true}
 
-  defp dep(:blue_heron_transport_usb, :hex),
-    do: {:blue_heron_transport_usb, ">= 0.0.0", only: :dev}
+  # defp dep(:blue_heron_transport_usb, :hex),
+  #   do: {:blue_heron_transport_usb, ">= 0.0.0", only: :dev}
 
-  defp dep(:blue_heron_transport_usb, :github),
-    do:
-      {:blue_heron_transport_usb,
-       github: "blue-heron/blue_heron_transport_usb", branch: "main", only: :dev, override: true}
+  # defp dep(:blue_heron_transport_usb, :github),
+  #   do:
+  #     {:blue_heron_transport_usb,
+  #      github: "blue-heron/blue_heron_transport_usb", branch: "main", only: :dev, override: true}
 
-  defp dep(:blue_heron_transport_usb, :path),
-    do:
-      {:blue_heron_transport_usb,
-       path: "~/dev/forks/blue_heron/blue_heron_transport_usb", only: :dev, override: true}
+  # defp dep(:blue_heron_transport_usb, :path),
+  #   do:
+  #     {:blue_heron_transport_usb,
+  #      path: "~/dev/forks/blue_heron/blue_heron_transport_usb", only: :dev, override: true}
 end
