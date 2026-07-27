a91dc45233342a238b0fc8b9b7cbf5bd12597d77
Jason Axelson <jason.axelson@gmail.com>
2026-07-26 16:06:17 -1000

    relax gettext

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule GoveePhx.MixProject do
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6 or ~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 1.0"},
+      {:gettext, "~> 0.6 or ~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
